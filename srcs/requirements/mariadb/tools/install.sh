8c671e36da6983629b2f39527d2c7cdff2caaa41
Matthieu Servagent <mservage@z3r8p3.42lyon.fr>
2022-03-16 16:48:47 +0100

    relou :(

--- srcs/requirements/mariadb/tools/install.sh
+++ srcs/requirements/mariadb/tools/install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+if [[! -d /var/lib/mysql/my_database]]; then
+
 service mysql start
+
+mysql -u root -e	"CREATE DATABASE my_database ;
+					CREATE USER '$USER' @ '%' IDENTIFIED BY '$USER_MDP' ;
+					GRANT ALL PRIVILEGES ON *.* TO '$USER' @ '%' ;
+					FLUSH PRIVILEGES ;
+					"
+mysqladmin -u root password "$ROOT_MDP"
+
+service mysql stop
+
+fi
+
+sh /usr/bin/mysqld_safe
